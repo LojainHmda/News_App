8837de9cb6a1feb41ef9081edee682ee2903814b
lojain <lojain2077@gmail.com>
2025-02-11 16:56:37 +0200

    exception handling

--- lib/Features/news_list.dart
+++ lib/Features/news_list.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/news_model.dart';
 
 class NewsList extends StatefulWidget {
   NewsList({super.key});
@@ -8,25 +9,70 @@ class NewsList extends StatefulWidget {
   State<NewsList> createState() => _NewsListState();
 }
 
- getNews() async {
-  Dio dio = Dio();
-  Response response = await dio.get(
-      "newsapi.org/v2/everything?q=tesla&from=2025-01-01&sortBy=publishedAt&apiKey=afe38ea1b2a94dcaa8fffa65c24d40f9");
-      return response.data;
-}
-
 class _NewsListState extends State<NewsList> {
-  List<dynamic> news = [];
+  List<NewsModel> listOfarticales = [];
+  bool isLoader = true;
+  String? errorMessage ;
+
+  void getNews() async {
+    Dio dio = Dio();try{
+    Response response = await dio.get(
+        "https://newsapi.org/v2/everything?q=apple&from=2025-02-06&to=2025-02-06&sortBy=popularity&apiKey=6065290155eb4c81ab086901c6b90599");
+
+    Map<String, dynamic> Apinews = response.data;
+    List<dynamic> news = Apinews['articles'];
+    List<NewsModel> listOfviews = [];
+
+    for (var item in news) {
+      NewsModel article = NewsModel(
+          source: item["source"]??"no sourc",
+          author: item["author"]??"no auther",
+          title: item["title"]??"no title",
+          description: item["description"]??"no desc",
+          url: item["url"]??"no url",
+          urlToImage: item["urlToImage"]?? "no Image",
+          publishedAt: item["publishedAt"]?? "no date",
+          content: item["content"]?? "no content");
+      listOfviews.add(article);
+    }
+    setState(() {
+      listOfarticales = listOfviews;
+      isLoader = false;
+    });}catch(err){
+      setState(() {
+            isLoader = false;
+     errorMessage = err.toString();
+
+      });
+  }
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getNews();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: ListView.builder(
-          itemCount: news.length,
-          itemBuilder: (context, index) {
-           news= getNews();
-            return Card(child: ListTile(title: Text("News")));
-          }),
+      body: isLoader
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : errorMessage!=null?  Center(
+              child:Text(errorMessage!),
+            ) : ListView.builder(
+              itemCount: listOfarticales.length,
+              itemBuilder: (context, index) {
+                return Card(
+                    child: ListTile(
+                        tileColor: Colors.teal,
+                        textColor: Colors.black,
+                        title: Text(listOfarticales[index].title ?? "null")));
+              }),
     );
   }
 }
---
+++ lib/models/news_model.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dio/dio.dart';
+
+class NewsModel {
+  Map? source;
+  String? author;
+  String? title;
+  String? description;
+  String? url;
+  String? urlToImage;
+  String? publishedAt;
+  String? content;
+  NewsModel({
+    required this.source,
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+}
